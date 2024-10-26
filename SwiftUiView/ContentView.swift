56018a1992a294dfa1ce50ffcdb363aef0bb9a50
seyamasan <kst_apr_24@icloud.com>
2024-10-27 00:40:03 +0900

    とりあえず、ナビゲーションバーにタイトルつけといた

--- SwiftUiView/ContentView.swift
+++ SwiftUiView/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Git Test!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Git Test!")
+            }
+            .padding()
+            
+            .navigationTitle("SwiftUI View")
         }
-        .padding()
     }
 }
 
