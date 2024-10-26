582e712baf0d73b299d2cb7b20ef82b3d9dc7774
seyamasan <kst_apr_24@icloud.com>
2024-10-26 22:12:14 +0900

    Initial Commit

---
+++ SwiftUiView/SwiftUiViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiViewApp.swift
+//  SwiftUiView
+//
+//  Created by Seyamasan on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
